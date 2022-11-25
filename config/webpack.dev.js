acb86b4f85997f4bfb6fad71db6e126e32432686
Yan Logan <yanlogans@gmail.com>
2022-11-25 16:40:39 +0300

    add buildPath to dev server

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,6 +1,11 @@
+const { buildPath } = require("./paths");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
 
 module.exports = merge(common, {
   mode: "development",
+
+  devServer: {
+    static: buildPath,
+  },
 });
