98b9a4bd5f9ea8dded561e9b07811b38eab4bf1f
Yan Logan <yanlogans@gmail.com>
2022-11-25 16:27:52 +0300

    configure paths

---
+++ config/paths.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+module.exports = {
+  srcPath: path.resolve(__dirname, "../src"),
+  buildPath: path.resolve(__dirname, "../dist"),
+  publicPath: path.resolve(__dirname, "../public"),
+};
