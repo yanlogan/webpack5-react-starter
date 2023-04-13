c0312e6b909e55e2cc7a478cf3b4a73997b53971
Yan Logan <yanlogans@gmail.com>
2023-04-13 18:45:36 +0300

    remove unnecessary code from App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+// import responsiveImageDesktop from "./assets/images/bg.png";
+// import responsiveImageMobile from "./assets/images/bg-mobile.png";
+
 const App = (props) => {
   return (
     <div>
       <h1>I'm working!</h1>
+      {/* <picture>
+        <source
+          media="(max-width: 750px)"
+          srcSet={responsiveImageMobile.srcSet}
+        />
+        <img
+          src={responsiveImageDesktop.src}
+          srcSet={responsiveImageDesktop.srcSet}
+          alt=""
+        />
+      </picture> */}
     </div>
   );
 };
